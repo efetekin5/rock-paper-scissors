4050d6beb90cf3240b04ec19ea9cee112ea88192
efe <121581223+efetekin5@users.noreply.github.com>
2023-04-08 17:50:19 +0300

    make playRound function

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const array = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
-    console.log(array[Math.floor(Math.random() * 3)])
+    return array[Math.floor(Math.random() * 3)];
 }
 
-getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    if((playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors') ||
+      (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper') ||
+      (playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock')
+    ) {
+        return 'You won';
+    } else if (playerSelection == computerSelection) {
+        return 'Tie';
+    }
+    else {
+        return 'You lost';
+    }
+}
+
+const playerSelection = prompt();
+let computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
